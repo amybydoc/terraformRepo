c936d41fc57b5883d3c730015ff059414fd4ccff
Amruth Raj <amruthb@in.startv.com>
2020-08-30 16:54:20 +0530

    Network Created from Terraform

---
+++ network.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region                  = "us-west-2"
+  shared_credentials_file = "/Users/amruthb/.aws/creds"
+  profile                 = "customprofile"
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
